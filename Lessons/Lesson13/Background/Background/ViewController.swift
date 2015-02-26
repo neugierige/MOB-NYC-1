97411406746c5fced7cb58122f3afcbd65918e60
Luyuan Xing <lxing.nyc@gmail.com>
2015-02-25 19:50:12 -0500

    general update

--- Lessons/Lesson13/Background/Background/ViewController.swift
+++ Lessons/Lesson13/Background/Background/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var backgroundSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,9 @@ class ViewController: UIViewController {
         self.backgroundSwitch.addTarget(self, action: "didChangeSwitch", forControlEvents: UIControlEvents.ValueChanged)
         
         self.backgroundSwitch.on = NSUserDefaults.standardUserDefaults().boolForKey("switchIsOn")
+        
         didChangeSwitch()
+        
         if let path = NSBundle.mainBundle().pathForResource("Athletes", ofType: "plist") {
             var array = NSArray(contentsOfFile: path)
             println(array)
--- Lessons/Lesson14/TodoPlists/TodoPlists/ViewController.swift
+++ Lessons/Lesson14/TodoPlists/TodoPlists/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UIAlertViewDelegate {
 
-    var items: [String] = []
+    var items: [[String:String]] = [] //[["Name": "SomeNameHere", "Content": "SomeContentHere"], ["Name": "AnotherName", "Content": "MoreContent"]]
     
     @IBAction func didTapAdd(sender: AnyObject) {
         var alert = UIAlertView(title: "Item Name?", message: "Enter an item name", delegate: self, cancelButtonTitle: "Dismiss", otherButtonTitles: "Add")
@@ -21,18 +21,47 @@ class ViewController: UIViewController, UITableViewDataSource, UIAlertViewDelega
     func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
         if buttonIndex != alertView.cancelButtonIndex {
             if let textInAlert = alertView.textFieldAtIndex(0)?.text {
-                self.items.append(textInAlert)
+                self.items.append(["Name": textInAlert, "Content": "none yet"])
                 self.tableView.reloadData()
+                saveNewItem()
             }
         }
     }
     
+    func saveNewItem() {
+        let convertedArray: NSArray = items
+        convertedArray.writeToURL(fileURL(), atomically: true)
+    }
+    
+    func fileURL() -> NSURL {
+        var someURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0] as NSURL
+        var filePath = someURL.URLByAppendingPathComponent("ToDoList.plist")
+        return filePath
+    }
+
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fileURL()
+        let savedArray = NSArray(contentsOfURL: fileURL())
+        self.items = (savedArray as Array)
+        
     }
 
+    //        if let documentPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as? NSURL {
+    //            if let string = NSBundle.mainBundle().pathForResource("ToDoList", ofType: "plist") {
+    //
+    //                //unwrap array
+    //                if let array = NSArray(contentsOfFile: string) {
+    //                    items = array as Array
+    //                }
+    //            }
+    //            let filePath = documentPath.URLByAppendingPathComponent("file.plist", isDirectory: false)
+    //        }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -49,11 +78,13 @@ class ViewController: UIViewController, UITableViewDataSource, UIAlertViewDelega
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as UITableViewCell!
         if cell == nil {
-            cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "myCell")
         }
-        var stringAtSpecifiedIndex = self.items[indexPath.row]
+        var stringAtSpecifiedIndex = self.items[indexPath.row]["Name"]
         cell.textLabel?.text = stringAtSpecifiedIndex
+        cell.detailTextLabel?.text = self.items[indexPath.row]["Content"]
         return cell
     }
+
 }
 
